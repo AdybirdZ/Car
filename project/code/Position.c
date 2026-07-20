f2eafec03e64a4b1ca991fcadd500d7c2ada973e
tc2077 <LucyloveDavid2076@outlook.com>
2026-07-20 11:05:33 +0800

    Position.c函数注释

--- project/code/Position.c
+++ project/code/Position.c
@@ -3,6 +3,10 @@
 float euler_angle[3] = {0, 0, 0};       // 下标0为横滚，下标1为俯仰，下标2为方向
 bool enable_position = true;
 
+/*
+函数功能：IMU陀螺仪初始化
+参数：无
+*/
 void Position_Init ()
 {
     if(!enable_position)
@@ -18,6 +22,10 @@ void Position_Init ()
     Position_Update();
 }
 
+/*
+函数功能：姿态数据更新，从IMU驱动读取最新的欧拉角，存入全局数组
+参数：无
+*/
 void Position_Update ()
 {
     euler_angle[ROLL] = imu660rc_roll;
